2fcd01d0fd52526bfca3c12f55e90e129a03bd79
Mohsen Fathipour <mohsenfathipour.com@gmail.com>
2023-01-11 12:38:01 +0330

    [user] passwords are hash now

--- user/app/Http/Controllers/UserController.php
+++ user/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -47,7 +48,7 @@ class UserController extends Controller
         $user->name = $data['name'];
         $user->email = $data['email'];
         $user->email_verified_at = now();
-        $user->password = $data['password'];
+        $user->password = Hash::make($data['password']);
 
         $user->save();
 
